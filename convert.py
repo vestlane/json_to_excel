cc75c299efb3406ee456ece65a38525712d9fb0c
Gabriele Falace <gabriele@vestlane.com>
2023-12-04 14:45:38 +0100

    some fix

--- convert.py
+++ convert.py
@@ -4,7 +4,7 @@ import time
 import os
 
 #take it by peeking into a request after login to prod
-SESSION_COOKIE = 'eyJpZCI6IjIyNjI0MDdiLWExYWU    ... 0cnVlfQ==; _ga=GA1.1.1267307625.1692372568; _ga_KTGLXV8B2H=GS1.1.1694078221.5.0.1694078221.0.0.0; csrftoken=UsaVHzNV8ozH5ynWT7kcp0JMhiPWoylLtHwVtEWCi968v4JXjFIXpEwr7DNvTlcP; sessionid=0hho7bjjxak6erp3pm309wce0v3bkxyf; _hjIncludedInSessionSample_2798930=0; _hjSession_2798930=eyJpZCI6IjgxM2RjYjAxLTZlNTQtNDgzMi1iNDU1LTZhMzJlNTQ5MTczYyIsImNyZWF0ZWQiOjE2OTUzMzEyMjYzOTIsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; _ga_SP6YTCCCW3=GS1.1.1695331226.32.1.1695331227.0.0.0'
+SESSION_COOKIE = 'eyJpZCI6IjIyNj . . . nIjp0cnVlfQ==; _ga=GA1.1.72899393.1697115589; _ga_KTGLXV8B2H=GS1.1.1700164054.7.0.1700164336.0.0.0; csrftoken=dTdhGah43kXZ26FoAQX9V7tc53hKHfvdzUed3tsFWmlMO8SpYqBUlbhu78H3o7Me; sessionid=t70ejgyb97x0ridmmdnbxihv9i4n12gk; _hjIncludedInSessionSample_2798930=0; _hjSession_2798930=eyJpZCI6ImYzYTEwNDM0LWRkNTgtNGRjZi1hMDg4LTMwODc1MGM2YWJlZSIsImNyZWF0ZWQiOjE3MDEyODc1NDE3MzUsImluU2FtcGxlIjpmYWxzZSwic2Vzc2lvbml6ZXJCZXRhRW5hYmxlZCI6ZmFsc2V9; _hjAbsoluteSessionInProgress=1; _ga_SP6YTCCCW3=GS1.1.1701287541.38.1.1701287623.0.0.0'
 PAGE_START = 1
 PAGE_END = 19
 
@@ -13,16 +13,7 @@ REQUEST = (
     '-H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" '
     '-H "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8" '
     '-H "Connection: keep-alive" '
-    '-H "Cookie: _hjSessionUser_2798930=eyJpZCI6IjIyNjI0MDdiLWExYWUtNTdmOC04YjdlLTVhMmU2ZDhiNjk2MyIsImNyZWF0ZWQiOjE2NjU1NzQ1NDExNzEsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.1.1267307625.1692372568; _ga_KTGLXV8B2H=GS1.1.1694078221.5.0.1694078221.0.0.0; csrftoken=UsaVHzNV8ozH5ynWT7kcp0JMhiPWoylLtHwVtEWCi968v4JXjFIXpEwr7DNvTlcP; sessionid=0hho7bjjxak6erp3pm309wce0v3bkxyf; _hjIncludedInSessionSample_2798930=0; _hjSession_2798930=eyJpZCI6ImI1YzQzMWVahLWFiOGUtNDA4My1iZGVjLTlmYTVhNDk1YjQyNiIsImNyZWF0ZWQiOjE2OTYyMzYwNTU2MTIsImluU2FtcGxlIjpmYWxzZSwic2Vzc2lvbml6ZXJCZXRhRW5hYmxlZCI6ZmFsc2V9; _hjAbsoluteSessionInProgress=0; _ga_SP6YTCCCW3=GS1.1.1696236055.33.1.1696236056.0.0.0" '
-    '-H "Sec-Fetch-Dest: document" '
-    '-H "Sec-Fetch-Mode: navigate" '
-    '-H "Sec-Fetch-Site: none" '
-    '-H "Sec-Fetch-User: ?1" '
-    '-H "Upgrade-Insecure-Requests: 1" '
-    '-H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36" '
-    '-H "sec-ch-ua: \\"Chromium\\";v=\\"116\\", \\"Not)A;Brand\\";v=\\"24\\", \\"Google Chrome\\";v=\\"116\\"" '
-    '-H "sec-ch-ua-mobile: ?0" '
-    '-H "sec-ch-ua-platform: \\"macOS\\"" '
+    '-H "Cookie: <SESSION_COOKIE>"'
     '--compressed'
 )
 
